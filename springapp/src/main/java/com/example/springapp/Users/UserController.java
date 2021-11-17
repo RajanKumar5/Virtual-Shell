50ad08a337d6df6f0bc1a770b49548baa27ba3c1
Rajan kumar <rajan.kumar.rk.0502@gmail.com>
2021-11-17 18:54:42 +0530

    backend completed for login & signup

---
+++ springapp/src/main/java/com/example/springapp/Users/UserController.java
@@ -0,0 +1,34 @@
+package com.example.springapp.Users;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@CrossOrigin(origins = "*")
+public class UserController {
+
+    @Autowired
+    private UserService userService;
+
+    @GetMapping("/users")
+    public List<UserModel> getAllUsers() {
+        return userService.getAllUsers();
+    }
+
+    @PostMapping("/signup")
+    public boolean addUser(@RequestBody UserModel user) {
+        return userService.addUser(user);
+    }
+
+    @PostMapping("/login")
+    public boolean findUser(@RequestBody UserModel user) {
+        return userService.findUser(user);
+    }
+
+}
---
+++ springapp/src/main/java/com/example/springapp/Users/UserService.java
@@ -0,0 +1,37 @@
+package com.example.springapp.Users;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+    @Autowired
+    UserRepository userRepository;
+
+    public List<UserModel> getAllUsers(){
+        List<UserModel> users = new ArrayList<>();
+        userRepository.findAll().forEach(users::add);
+        return users;
+    }
+
+    public boolean addUser(UserModel user){
+        if (userRepository.findById(user.getEmail()).isEmpty()) {
+            userRepository.save(user);
+            return true;
+        }
+        return false;
+    }
+
+    public boolean findUser(UserModel user) {
+        if (!userRepository.findById(user.getEmail()).isEmpty()) {
+            UserModel dataUser = userRepository.findById(user.getEmail()).get();
+            String username = dataUser.getEmail();
+            String password = dataUser.getPassword();
+            return username.equals(user.getEmail()) && password.equals(user.getPassword());
+        }
+        return false;
+    }
+}
